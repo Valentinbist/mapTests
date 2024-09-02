4972835a676f3a5cdb2b0d3cfdb4c180fed17e2d
valentin <valentin.bister@gmail.com>
2024-09-02 13:34:41 +0200

    adding first test

--- geoapp/tests.py
+++ geoapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+class JWTGenerationTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_jwt_generation(self):
+        # Generate JWT for the test user
+        refresh = RefreshToken.for_user(self.user)
+        access_token = str(refresh.access_token)
+
+        # Verify the access token is not empty
+        self.assertNotEqual(access_token, '')
+
+        # Decode the token to verify its payload
+        decoded_payload = refresh.access_token.payload
+        self.assertEqual(decoded_payload['user_id'], self.user.id)
