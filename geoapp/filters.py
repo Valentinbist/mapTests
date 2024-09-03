59cac562cc2f0fda9164c51d54865b693d9c2101
valentin <valentin.bister@gmail.com>
2024-09-03 13:13:04 +0200

    add search and pagination to the edit page

--- geoapp/filters.py
+++ geoapp/filters.py
@@ -1,29 +1,14 @@
 import django_filters
 from django.contrib.gis.geos import Polygon
-from geoapp.models import FeatureCollection, Feature
-
-"""
-class FeatureFilter(django_filters.FilterSet):
-    print("filter")
-    bbox = django_filters.CharFilter(method='filter_bbox')
-
-    class Meta:
-        model = FeatureCollection
-        fields = ['bbox']
-
-    def filter_bbox(self, queryset, name, value):
-        minx, miny, maxx, maxy = map(float, value.split(','))
-        print(minx, miny, maxx, maxy)
-        bbox = Polygon.from_bbox((minx, miny, maxx, maxy))
-        return queryset.filter(features__geometry__intersects=bbox) #53.15,6,53.9,7
-    """ # the stupid one operation on the collection...
+from geoapp.models import Feature
 
 class FeatureFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='properties__name', lookup_expr='icontains')
     bbox = django_filters.CharFilter(method='filter_bbox')
 
     class Meta:
         model = Feature
-        fields = ['bbox']
+        fields = ['name', 'bbox']
 
     def filter_bbox(self, queryset, name, value):
         minx, miny, maxx, maxy = map(float, value.split(','))
